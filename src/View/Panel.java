5f33a74c3a1ec684ec6bf4590c329c64229d12a0
eileen0904 <xinlingzhang04@gmail.com>
2025-04-03 02:41:48 +0800

    modify button

--- src/View/Panel.java
+++ src/View/Panel.java
@@ -13,14 +13,62 @@ public class Panel extends JPanel {
         this.setLayout(new GridLayout(6, 1));
         canvas = Canvas.getInstance();
 
-        String[] buttons = { "select", "association line", "generalization line", "composition line", "class", "use case" };
-        for(String buttonName : buttons) {
-            JButton button = new JButton(buttonName);
+        String[] buttonNames = { "select", "association line", "generalization line", "composition line", "rect", "oval" };
+        String[] iconPaths = {
+                "icons/select.png",
+                "icons/association.png",
+                "icons/generalization.png",
+                "icons/composition.png", 
+                "icons/rect.png",
+                "icons/oval.png"
+        };
+
+        int buttonIconSize = 60;
+
+        for(int i = 0; i < buttonNames.length; i++) {
+            JButton button = new JButton();
+
+            try {
+                ImageIcon icon = new ImageIcon(iconPaths[i]);
+
+                int originalWidth = icon.getIconWidth();
+                int originalHeight = icon.getIconHeight();
+
+                double widthRatio = (double) buttonIconSize / originalWidth;
+                double heightRatio = (double) buttonIconSize / originalHeight;
+                double ratio = Math.min(widthRatio, heightRatio);
+
+                int scaledWidth = (int) (originalWidth * ratio);
+                int scaledHeight = (int) (originalHeight * ratio);
+
+                Image scaledImage = icon.getImage().getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
+
+                button.setIcon(new ImageIcon(scaledImage));
+            } 
+            catch(Exception e) {
+                button.setText(buttonNames[i]);
+                e.printStackTrace();
+            }
+
+            // 設定工具提示以顯示按鈕名稱
+            button.setToolTipText(buttonNames[i]);
+
+            final String buttonName = buttonNames[i];
             button.addActionListener((ActionEvent e) -> {
                 canvas.setMode(buttonName);
                 button.setBackground(Color.GRAY);
+
+                // 重設其他按鈕的顏色
+                for(Component comp : getComponents()) {
+                    if (comp instanceof JButton && comp != button) {
+                        comp.setBackground(Color.WHITE);
+                    }
+                }
             });
-            button.setBackground(Color.white);
+
+            button.setBackground(Color.WHITE);
+            button.setBorderPainted(true);
+            button.setFocusPainted(false);
             this.add(button);
         }
     }
